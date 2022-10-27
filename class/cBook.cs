87931081601b585afaf36d4134eab60384f70093
sampr23 <sampr2323@gmail.com>
2022-10-26 21:10:10 -0400

    last commit

--- class/cBook.cs
+++ class/cBook.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace library
 {
@@ -60,7 +54,7 @@ namespace library
             {
                 html = reader.ReadToEnd();
             }
-            Console.WriteLine(html);
+
             return html;
         }
     }
